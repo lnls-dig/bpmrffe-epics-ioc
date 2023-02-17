d72585341d1982dbae28f5c1341754a8fa8555ab
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-02-17 15:09:10 -0300

    reset: Add support for firmware reset command
    Reset command has been implemented based on the previous repository
    database file. Because of that, PV naming here is being kept, even
    though it is not following the pattern used at LNLS of having the `-Cmd`
    prefix for commands. It shall be updated in a future patch, which
    changes all PV names at once.
    
    Related the renaming aforementioned, the read-back record has not been
    included herein, as it does not follow the standard but it is not really
    used.
    
    As this record does not really have a binary state, the label for its
    zero value has been renamed to `reset`. On the other hand, `ONAM` has
    been removed because it is not used elsewhere.

--- iocBoot/iocBPMRFFEApp/st.cmd
+++ iocBoot/iocBPMRFFEApp/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/BPMRFFE
 
-## You may have to change BPMRFFE to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -11,18 +8,17 @@ cd "${TOP}"
 dbLoadDatabase "dbd/BPMRFFE.dbd"
 BPMRFFE_registerRecordDeviceDriver pdbbase
 
+## Setup environment variables
+epicsEnvSet("P", "${EPICS_PV_AREA_PREFIX}")
+epicsEnvSet("R", "${EPICS_PV_DEVICE_PREFIX}")
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/BPMRFFEApp/Db")
+epicsEnvSet("PORT", "BPMRFFE")
+
+## Set up address
+drvAsynIPPortConfigure("$(PORT)", "10.20.21.36:9001", 0, 0, 0)
+
 ## Load record instances
-dbLoadTemplate "db/user.substitutions"
-dbLoadRecords "db/dbSubExample.db", "user=henriquesimoes"
-
-## Set this to see messages from mySub
-#var mySubDebug 1
-
-## Run this to trace the stages of iocInit
-#traceIocInit
+dbLoadRecords("$(TOP)/db/bpmrffe.db", "P=$(P), R=$(R), PORT=$(PORT)")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
-## Start any sequence programs
-#seq sncExample, "user=henriquesimoes"
